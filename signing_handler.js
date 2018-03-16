5c861cb79134c74560aeec864fb5ff85b0915ef5
nubis <yo@nubis.im>
2018-03-16 12:34:29 -0300

    signing_handler.js

--- signing_handler.js
+++ signing_handler.js
@@ -1,7 +1,7 @@
 import * as device from './device.js'
 import * as helpers from './helpers.js'
 import * as bitcoin from 'bitcoinjs-lib'
-import {showError, loading, notLoading} from './messages.js'
+import {showError, showSuccess, loading, notLoading} from './messages.js'
 import {update_epidemic} from './lib/update_epidemic.js'
 var bip32 = require('bip32-path')
 var _ = require('lodash')
@@ -14,24 +14,47 @@ export function signingHandler(){
 		$virus: update_epidemic,
     class: 'form',
     _transaction_json: '',
+    _rawtx: null,
+    $update(){
+      if(this._rawtx){
+        this.$build({ 
+          class: 'alert alert-secondary',
+          $type: 'textarea',
+          cols: 100,
+          $text: this._rawtx })
+      }
+    },
     $$: [
       { $tag: '.form-group', $$: [
         { $tag: 'label', $text: 'Transaction JSON' },
         { $tag: 'textarea#tansaction_json.form-control',
           name: 'transaction_json',
           rows: 15,
-          $update(){ this.$text = JSON.stringify(this._transaction_json, true, '  ') }
+          $update(){
+            this.$text = JSON.stringify(this._transaction_json, true, '  ')
+          }
         },
       ]},
       { $tag: 'button.btn.btn-default.btn-block',
-        $text: 'Load example transaction',
-        onclick(){ this._transaction_json = exampleTransactionJson() }
+        $text: 'Load transaction',
+        onclick(){ this._transaction_json = exampleSpendAddressJson() }
+      },
+      { $tag: 'button.btn.btn-default.btn-block',
+        $text: 'Load multisig transaction',
+        onclick(){ this._transaction_json = exampleSpendMultisigJson() }
       },
       { $tag: 'button.btn.btn-primary.btn-block',
         $text: 'Sign transaction',
+        _handle_signing_result(result){
+          this._transaction_json = result.json
+          this._rawtx = result.rawtx
+          if(result.done){
+            showSuccess("All signed, try to propagate rawtx")
+          }
+        },
         onclick(){
           signTransaction(this._transaction_json, 'testnet')
-            .then((signed) => this._transaction_json = signed )
+            .then(this._handle_signing_result)
         }
       }
     ]
@@ -45,54 +68,104 @@ function signTransaction(json, coin){
       .then((res) => {
         let signed = res.message.serialized.serialized_tx
         let signatures = res.message.serialized.signatures
-        return d.session.getPublicKey([]).then( (result) => {
-          let publicKey = result.message.node.public_key
-          _.each(json.inputs, (input, inputIndex) => {
-            let signatureIndex = _.findIndex(input.multisig.pubkeys,
-              (p) => p.node.public_key == publicKey)
-            input.multisig.signatures[signatureIndex] = signatures[inputIndex]
+        if(_.some(json.inputs, (i) => i.multisig )) {
+          return d.session.getPublicKey([]).then( (result) => {
+            let publicKey = result.message.node.public_key
+            _.each(json.inputs, (input, inputIndex) => {
+              let signatureIndex = _.findIndex(input.multisig.pubkeys,
+                (p) => p.node.public_key == publicKey)
+              input.multisig.signatures[signatureIndex] = signatures[inputIndex]
+            })
+
+            let done = _.every(json.inputs, (i) => {
+              _.filter(i.multisig.signatures, (s) => s.length > 0).length >= i.multisig.m
+            })
+
+            notLoading()
+            return {json: json, done: done, rawtx: signed}
           })
-					notLoading()
-          return json
-        })
+        }else{
+          return { json: json, done: true, rawtx: signed }
+        }
       })
   })
 }
 
-function exampleTransactionJson(){
-	/*
-	Device seed: 'custodian popup test'
-		singlesig:
-			path: '0/1/2/3'
-			address: 'mgYDL9xvE9bDAXQdWseNttP5V6iaRmBVZK'
-			
-			utxo: ['cb7ae6beaeda9591ec9be0d6de8d363e57d4a5f4dc4bf79a33fb6c942c09e02b', 0]
+function exampleSpendAddressJson(){
+  /*
+  This transaction sends money from a regular (paytoaddress) address to itself.
 
-		multisig:
-			address: '2NFYkN6NcY7YV9gpEiahadmsT5h38t8hK99'
-			path: '0/1/2/3'
-			root_extended_keys:
-				one: tpubD6NzVbkrYhZ4YSh1zgHc1L2fNXQmSZM1FEbVFpNGzK9J1GDuhRnfoLUA7Unzq44qHVviVtyKdfLjnJYiuTUTjYAJt6Un4svFfRPb7m6TvZk
-				two: tpubD6NzVbkrYhZ4YBZEKjx5MH2yTFykDGATeS6qJDwRSCz8PZp7jSd3TZKZzAaWz1CZxrupuTyDue7L3gxC62CMSvxobDZN6tF1q29vd2WroBm
-				three: tpubD6NzVbkrYhZ4WsV7xWDG6J3oCpgai4uAmEu9A4RDq3hsmEJjaBKiKtNvpSQGQ4zFyC5KXiVd3pc6Q7DqJNPZ7oQUY2vvHPzUPo1R4cUqPqk
-			utxo: ['c6199535c5e2bbf6437c87dfa4d8b6b16f6f9dae66eaa3205cb7044af974d6a8', 0]
-	*/
+	device_seed: 'custodian popup test'
+  path: '0/1/2/3'
+  address: 'mgYDL9xvE9bDAXQdWseNttP5V6iaRmBVZK'
+  utxo: ['cb7ae6beaeda9591ec9be0d6de8d363e57d4a5f4dc4bf79a33fb6c942c09e02b', 0]
+  https://testnet.blockexplorer.com/api/tx/cb7ae6beaeda9591ec9be0d6de8d363e57d4a5f4dc4bf79a33fb6c942c09e02b
+  */
 	return {
 		outputs: [
 			{ script_type: 'PAYTOADDRESS',
 				address: 'mgYDL9xvE9bDAXQdWseNttP5V6iaRmBVZK',
 				amount: 130000000
-			}/*,
-			{ script_type: 'PAYTOSCRIPTHASH',
-				address: '2NFYkN6NcY7YV9gpEiahadmsT5h38t8hK99',
-				amount: 64900000
-			}*/
+			}
 		],
 		inputs: [
 			{ address_n: [0,1,2,3],
 				prev_hash: 'cb7ae6beaeda9591ec9be0d6de8d363e57d4a5f4dc4bf79a33fb6c942c09e02b',
 				prev_index: 0
-			}/*,
+			},
+		],
+		transactions: [
+			{ hash: "cb7ae6beaeda9591ec9be0d6de8d363e57d4a5f4dc4bf79a33fb6c942c09e02b",
+				version: 1,
+				lock_time: 0,
+				inputs: [
+					{ prev_hash: "f52d8e97d39d0daa3d324c516a1e989975df74cc5ef6bdfa33e151310b22e176",
+						prev_index: 1,
+						sequence: 4294967295,
+						script_sig: "160014b5cfcd65d0764a18ae74c583c2b341cb97335323" // HEX, not ASM.
+					},
+				],
+				bin_outputs: [
+					{ amount: 130000000,
+						script_pubkey: "76a9140b3517e6562623042f7ae1fa9da19d3106841a8a88ac" // HEX, not ASM.
+					},
+					{ amount: 6922866917, 
+						script_pubkey: "a91490a8548f36918a89d39d7eb0a8c8b3f095478e8987" // HEX, not ASM.
+					}
+				]
+			}
+		]
+	}
+}
+
+function exampleSpendMultisigJson(){
+	/*
+  This transaction sends money from a multisig address to itself.
+
+	device seed: 'custodian popup test'
+  path: '0/1/2/3'
+  address: '2NFYkN6NcY7YV9gpEiahadmsT5h38t8hK99'
+  utxo: ['c6199535c5e2bbf6437c87dfa4d8b6b16f6f9dae66eaa3205cb7044af974d6a8', 0]
+  utxo_url: https://testnet.blockexplorer.com/api/tx/c6199535c5e2bbf6437c87dfa4d8b6b16f6f9dae66eaa3205cb7044af974d6a8
+  root_extended_public_keys:
+    0:
+      passphrase: 'one'
+      xpub: 'tpubD6NzVbkrYhZ4YSh1zgHc1L2fNXQmSZM1FEbVFpNGzK9J1GDuhRnfoLUA7Unzq44qHVviVtyKdfLjnJYiuTUTjYAJt6Un4svFfRPb7m6TvZk'
+    1:
+      passphrase: 'two'
+      xpub: 'tpubD6NzVbkrYhZ4YBZEKjx5MH2yTFykDGATeS6qJDwRSCz8PZp7jSd3TZKZzAaWz1CZxrupuTyDue7L3gxC62CMSvxobDZN6tF1q29vd2WroBm'
+    2:
+      passphrase: 'three'
+      xpub: 'tpubD6NzVbkrYhZ4WsV7xWDG6J3oCpgai4uAmEu9A4RDq3hsmEJjaBKiKtNvpSQGQ4zFyC5KXiVd3pc6Q7DqJNPZ7oQUY2vvHPzUPo1R4cUqPqk'
+	*/
+	return {
+		outputs: [
+			{ script_type: 'PAYTOSCRIPTHASH',
+				address: '2NFYkN6NcY7YV9gpEiahadmsT5h38t8hK99',
+				amount: 64900000
+			}
+		],
+		inputs: [
 			{ address_n: [0,1,2,3],
 				prev_hash: 'c6199535c5e2bbf6437c87dfa4d8b6b16f6f9dae66eaa3205cb7044af974d6a8',
 				prev_index: 0,
@@ -130,7 +203,7 @@ function exampleTransactionJson(){
 						}
 					]
 				}
-			}*/
+			}
 		],
 		transactions: [
 			{ hash: "cb7ae6beaeda9591ec9be0d6de8d363e57d4a5f4dc4bf79a33fb6c942c09e02b",
@@ -151,7 +224,7 @@ function exampleTransactionJson(){
 						script_pubkey: "a91490a8548f36918a89d39d7eb0a8c8b3f095478e8987" // HEX, not ASM.
 					}
 				]
-			}/*,
+			},
 			{ hash: "c6199535c5e2bbf6437c87dfa4d8b6b16f6f9dae66eaa3205cb7044af974d6a8",
 				version: 1,
 				lock_time: 0,
@@ -159,7 +232,7 @@ function exampleTransactionJson(){
 					{ prev_hash: "3ef7f7ce2ecac486c65cbfd23fae143b87d5425f5ea13e47e257814b836eff7c",
 						prev_index: 1,
 						sequence: 4294967295,
-						script_sig: "7c94224a4e62aabf6da3360a62000d3c4e38d20" // HEX, not ASM.
+						script_sig: "160014a7c94224a4e62aabf6da3360a62000d3c4e38d20" // HEX, not ASM.
 					},
 				],
 				bin_outputs: [
@@ -170,7 +243,7 @@ function exampleTransactionJson(){
 						script_pubkey: "a914c2585ba5b2f3c27063b8f0a36e376eefcfcd4bff87" // HEX, not ASM.
 					}
 				]
-			}*/
+			}
 		]
 	}
 }
