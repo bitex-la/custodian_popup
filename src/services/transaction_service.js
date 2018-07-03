6ef318ecf04416521253724d993e450779f89ed3
werner <werner@yahoo.es>
2018-07-03 15:04:45 -0300

    add jQuery.ajax method for broadcast transaction

--- src/services/transaction_service.js
+++ src/services/transaction_service.js
@@ -3,7 +3,15 @@ import {baseService} from './base_service.js'
 export function transactionService() {
   return {
     broadcast (hash, successCallback, errorCallback) {
-      baseService().postToNode('/transactions/broadcast', hash, successCallback, errorCallback)
+      jQuery.ajax({
+        method: 'POST',
+        url: `${nodeUrl}/transactions/broadcast`,
+        contentType: 'application/json; charset=utf-8',
+        data: hash,
+        crossDomain: true,
+        success: successCallback,
+        error: errorCallback
+      })
     }
   }
 }
