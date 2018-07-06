8fef6d1fd78ace05795dded8b3db05d63946eab1
werner <werner@yahoo.es>
2018-07-06 16:38:03 -0300

    Tests basic node creation

--- test/index.js
+++ test/index.js
@@ -1,8 +1,42 @@
-import { Selector } from 'testcafe'
+import { Selector, RequestMock } from 'testcafe'
 
 fixture `Getting Started`
     .page `http://localhost:9966`
 
-test('My first test', async t => {
+var mock = RequestMock()
+  .onRequestTo(/\/hd_wallets\//)
+  .respond((req, res) => {
+    console.log(res)
+    res.setBody({data: { attributes: {  }, id: '123', type: 'hd_wallet' }})
+  })
+
+test('Check navbar', async t => {
     await t.expect(Selector('.nav-item:first-child a').innerText).eql('Signing')
+    await t.expect(Selector('.nav-item:nth-child(2) a').innerText).eql('Multisig setup')
+    await t.expect(Selector('.nav-item:nth-child(3) a').innerText).eql('Load device')
+    await t.expect(Selector('.nav-item:nth-child(4) a').innerText).eql('Debugger')
+    await t.expect(Selector('.nav-item:nth-child(5) a').innerText).eql('Wallets')
+})
+
+test
+  .requestHooks(mock)
+  ('Creates a Node', async t => {
+    const selectNetwork = Selector('#multisig_setup_network')
+    const nodeList = Selector('.hd-nodes')
+
+    const selectWallet = Selector('#wallets select')
+
+    await t
+      .click('a[href="#tab_multisig_setup"]')
+      .typeText('#multisig_setup_xpub', 'tpubD6NzVbkrYhZ4YSh1zgHc1L2fNXQmSZM1FEbVFpNGzK9J1GDuhRnfoLUA7Unzq44qHVviVtyKdfLjnJYiuTUTjYAJt6Un4svFfRPb7m6TvZk')
+      .click(selectNetwork)
+      .click(selectNetwork.find('option').withText('testnet'))
+      .click('.add-node-group > button')
+      .expect(nodeList.exists).ok()
+      .expect(nodeList.textContent).contains('mxZpWbpSVtJoLHU2ZSC75VTteKc4F7RkTn')
+      .click('.wallet-creation > button')
+      .click('a[href="#tab_wallets"]')
+      .click(selectWallet)
+      .click(selectWallet.find('option').withText('Hd'))
+      .expect(Selector('.wallets-table').textContent).contains('mxZpWbpSVtJoLHU2ZSC75VTteKc4F7RkTn')
 })
