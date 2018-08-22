540da3bcc307e1fedc7ae169107afab9cb1a892f
werner <werner@yahoo.es>
2018-08-22 16:55:28 -0300

    Check original value for rsk address balance (if fails there is no internet connection or address was used)

--- tests/rsk.js
+++ tests/rsk.js
@@ -1,25 +1,10 @@
 import { Selector } from 'testcafe'
 import { mockTrezor } from './trezor.js'
-import { mockXmlHttpRequest } from './xmlHttpRequest.js'
-
-// const mock = RequestMock().onRequestTo(request => {
-//   return /mycrypto/.test(request.url) &&
-//          request.method === 'POST' &&
-//          request.body === '{"jsonrpc":"2.0","id":7,"method":"eth_getBalance","params":["0xb5ae11144f988735aecf469b96b72f979736dbcc","latest"]}' &&
-//          request.headers['content-type'] === 'application/json'
-// }).respond('20000')
-
-// const mock = RequestMock().onRequestTo(/mycrypto/).respond((req, res) => {
-//   res.headers['Access-Control-Allow-Origin'] = '*'
-//   res.statusCode = '200'
-//   res.setBody("{'jsonrpc': '2.0', 'id': 2, 'result': '0x4E20'}")
-// })
 
 fixture(`Testing Rsk transactions`).page(`http://localhost:9966`)
 
 test('Creates and test a rsk transaction', async t => {
   mockTrezor(t)
-  mockXmlHttpRequest(t)
 
   const selectNetwork = Selector('#setup_network')
   const selectPath = Selector('#derivation_path')
@@ -42,6 +27,7 @@ test('Creates and test a rsk transaction', async t => {
     .typeText('input[id="from-address-rsk"]', '0xb5ae11144f988735aecf469b96b72f979736dbcc')
     .typeText('input[id="to-address-rsk"]', '0x92ca912e41f9659b86f3147ef6f5abd6e93658df')
     .click('button[data-id="add-balance"]')
+    .expect(Selector('#amount-rsk').value).eql('1000000000000000')
     .click('button[data-id="create-rsk-modal-tx"]')
     .click(selectPath)
     .click(selectPath.find('option').withText("Testnet m/44'/1'/0'/0'/0"))
