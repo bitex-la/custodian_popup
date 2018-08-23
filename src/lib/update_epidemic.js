b26fb0f3cd810d457e42d9f21628d9fd40fc8f11
werner <werner@yahoo.es>
2018-08-23 12:07:17 -0300

    Fix eslint warnings on update_epidemic

--- src/lib/update_epidemic.js
+++ src/lib/update_epidemic.js
@@ -1,17 +1,17 @@
-export function updateEpidemic(component){
-	let recursive_update = (node) => {
-		for(let n of node.children){
-			n.$update && n.$update()
-			recursive_update(n)
-		}
-	}
+export function updateEpidemic (component) {
+  let recursiveUpdate = (node) => {
+    for (let n of node.children) {
+      n.$update && n.$update()
+      recursiveUpdate(n)
+    }
+  }
 
-	let old_update = component.$update
+  let oldUpdate = component.$update
 
-	component.$update = function(){
-		old_update && old_update.call(this)
-		recursive_update(this)
-	}
+  component.$update = function () {
+    oldUpdate && oldUpdate.call(this)
+    recursiveUpdate(this)
+  }
 
-	return component
+  return component
 }
