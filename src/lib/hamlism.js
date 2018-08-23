7e1bfdbba22b1cee3e75c43399d4a5abe508fdce
werner <werner@yahoo.es>
2018-08-23 13:25:20 -0300

    Fix eslints warnings for hamlism

--- src/lib/hamlism.js
+++ src/lib/hamlism.js
@@ -1,25 +1,25 @@
-export function hamlism(component){
-  if(component.$$){
-    component.$components = component.$$;
+export function hamlism (component) {
+  if (component.$$) {
+    component.$components = component.$$
     delete component.$$
   }
 
-  if(component.$components){
+  if (component.$components) {
     component.$components = component.$components.map(hamlism)
   }
 
   let tag = component.$tag
-  if(!tag) return component
+  if (!tag) return component
 
   let selectors = tag.split(' ')
   expandSelector(component, selectors.pop())
 
-  return selectors.reduceRight(function(child, selector){
+  return selectors.reduceRight(function (child, selector) {
     return expandSelector({$components: [child]}, selector)
   }, component)
 }
 
-function expandSelector(component, selector){
+function expandSelector (component, selector) {
   let parts = selector.match(/([a-zA-Z0-9]*)([#a-zA-Z0-9-_]*)([.a-zA-Z0-9-_]*)/)
   if (parts[1]) component.$type = parts[1]
   if (parts[2]) component.id = parts[2].substring(1)
