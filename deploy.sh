587600483251072afbeafe97e70f3a53701521d2
werner <werner@yahoo.es>
2018-09-26 15:05:28 -0300

    use cli in deploy

--- deploy.sh
+++ deploy.sh
@@ -3,6 +3,6 @@
 USER=$1
 IP=$2
 
-node_modules/webpack-cli/bin/webpack.js --mode=production
+node_modules/webpack-cli/bin/cli.js --mode=production
 trezor-agent $HOME/.ssh/config -- scp src/bootstrap.css src/popup.css $USER@$IP:/home/ubuntu/apps/custodian_popup/src
 trezor-agent $HOME/.ssh/config -- scp build/popup.bundle.js build/popup.bundle.js.map index.html config_signed.bin $USER@$IP:/home/ubuntu/apps/custodian_popup 
