c6127b83e2bc931cf170ea5a2946db74701af39a
werner <werner@yahoo.es>
2018-11-22 17:21:07 -0300

    Load wallets from server

--- src/services/wallet_service.ts
+++ src/services/wallet_service.ts
@@ -1,9 +1,18 @@
 import { baseService } from './base_service';
 import { Config } from '../config';
 
+interface JsonApiWallet {
+  data: [{
+    attributes: {
+      balance: number,
+      label: string
+    }
+  }]
+}
+
 export function WalletService (config: Config) {
   return {
-    list (url: string): Promise<{ data: { } }> {
+    list (url: string): Promise<JsonApiWallet> {
       return new Promise(
         resolve => {
           return resolve(baseService(config)
